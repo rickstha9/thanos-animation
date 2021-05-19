ed7275f4d3564748a06a1224eab895cbf2a1f661
Rikesh Shrestha <rickxtha@gmail.com>
2021-05-19 14:06:25 +0545

    thanos animation done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// var preloader=document.getElementById('overlayer');
+// function myFunction(){
+//   preloader.style.display='none';
+// }
+
+setTimeout(function(){
+  $('#overlayer').fadeToggle();
+  },12000);
+
